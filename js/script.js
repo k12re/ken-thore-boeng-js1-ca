61fa2c5a2acf7ed0e5abb2b39fd2d96024301279
Ken Thore <kenthore@live.no>
2022-09-05 22:29:19 +0200

    small test

--- js/script.js
+++ js/script.js
@@ -7,26 +7,20 @@ const movieContainer = document.querySelector(".movie");
 
 async function callAPI() {
 
-    
-
     const response = await fetch(url);
     const json = await response.json();
     //console.log(json.data.memes);
     const results = json.data;
     console.log(results);
 
-  
     //movieContainer.innerHTML = "No movies";
-
 
     for (let i = 0; i < results.length; i++) {
         resultsContainer.innerHTML += `<a href="details.html?id=${results[i]._id}">
                                         <h2 class="result">Name: ${results[i].name}</h2>
                                         <img class="image" src="${results[i].imageUrl}" alt="${results[i].name}"/>
                                         <li class="films">Films:
-                                            <ul class="movie">${results[i].films[0]}</ul>
-                                            <ul class="movie">${results[i].films[1]}</ul>
-                                            <ul class="movie">${results[i].films[2]}</ul>
+                                            <ul class="movie">${results[i].films}</ul>
                                         </li>
                                         </a>`;
                                         //console.log(results[i].id);  
@@ -34,7 +28,6 @@ async function callAPI() {
                                    
     }
 
-    
 }
 
 callAPI()
